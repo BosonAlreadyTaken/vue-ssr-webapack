192348e9d782a5fbacd3e1fe86f1341a185db91c
showa11 <shibushishang@gmail.com>
2018-06-25 08:44:12 +0800

    first commit

---
+++ build/webpack.config.client.js
@@ -0,0 +1,91 @@
+const path = require('path')
+const HTMLPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const ExtractPlugin = require('extract-text-webpack-plugin')
+const baseConfig = require('./webpack.config.base')
+
+const isDev = process.env.NODE_ENV === 'development'
+
+let config;
+
+const defaultPlugins = [
+    new webpack.DefinePlugin({
+        'process.env': {
+            NODE_ENV: isDev ? '"development"' : '"production"'
+        }
+    }),
+    new HTMLPlugin()
+]
+
+if (isDev) {
+    config = merge(baseConfig, {
+        module: {
+            rules: [{
+                test: /\.styl/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            sourceMap: true,
+                        }
+                    },
+                    'stylus-loader'
+                ]
+            }]
+        },
+        devtool: '#cheap-module-eval-source-map',
+        devServer: {
+            port: 8000,
+            host: '0.0.0.0',
+            overlay: {
+                errors: true,
+            },
+            hot: true
+        },
+        plugins: defaultPlugins.concat([new webpack.HotModuleReplacementPlugin(), 
+            new webpack.NoEmitOnErrorsPlugin()
+        ])
+    })
+} else {
+    config = merge(baseConfig, {
+        entry: {
+            app: path.join(__dirname, '../src/index.js'),
+            vendor: ['vue']
+        },
+        output: {
+            filename: '[name].[chunkhash:8].js'
+        },
+        module: {
+            rules: [{
+                test: /\.styl/,
+                use: ExtractPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        'css-loader',
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                sourceMap: true,
+                            }
+                        },
+                        'stylus-loader'
+                    ]
+                })
+            }]
+        },
+        plugins: defaultPlugins.concat([
+            new ExtractPlugin('styles.[contentHash:8].css'),
+            new webpack.optimize.CommonsChunkPlugin({
+                name: 'vendor'
+            }),
+            new webpack.optimize.CommonsChunkPlugin({
+                name: 'runtime'
+            })
+        ])
+    })  
+}
+
+module.exports = config;
---
+++ src/layout/header.vue
@@ -0,0 +1,23 @@
+<template>
+    <header class="main-header">
+        <h1>BOSN'S TODO</h1>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: "header"
+    }
+</script>
+
+<style lang="stylus" scoped>
+.main-header {
+    text-align center
+    h1 {
+        font-size 100px
+        color: rgba(175, 47, 47, .4)
+        font-weight: 300
+        margin: 20px;
+    }
+}
+</style>
